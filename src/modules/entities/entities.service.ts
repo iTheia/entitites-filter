9d0a922a80c8c4b784968ed2aa64989e42094e01
marlon <marlon@miflink.com>
2022-02-08 19:50:42 -0600

    feature/add redis cache to entities/filter

--- src/modules/entities/entities.service.ts
+++ src/modules/entities/entities.service.ts
@@ -2,15 +2,30 @@ import { ENTITIES_URL } from '@const/data';
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { FilterBodyDto } from './dto';
-
+import { RedisService } from 'nestjs-redis';
+import { Redis } from 'ioredis';
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class EntitiesService {
+  private client: Redis;
+  constructor(
+    private readonly redis: RedisService,
+    private config: ConfigService,
+  ) {
+    this.client = this.redis.getClient(this.config.get('redis.name'));
+  }
   async filter(dto: FilterBodyDto) {
     const response = [];
     for (let index = dto.startId; index <= dto.endId; index++) {
-      const { data } = await axios.get(ENTITIES_URL + index);
-      delete data.data.ipAddress;
-      response.push(data.data);
+      const registered = await this.client.get(index.toString());
+      let body = registered ? JSON.parse(registered) : {};
+      if (!registered) {
+        const { data } = await axios.get(ENTITIES_URL + index);
+        delete data.data.ipAddress;
+        body = data.data;
+        await this.client.set(index.toString(), JSON.stringify(body));
+      }
+      response.push(body);
     }
     return response;
   }
