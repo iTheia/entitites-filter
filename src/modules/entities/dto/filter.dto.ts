8982e9b236bed6926240b962e2696fc027d07f87
marlon <marlon@miflink.com>
2022-02-08 18:35:29 -0600

    feature/add validation to entitites/filter body

--- src/modules/entities/dto/filter.dto.ts
+++ src/modules/entities/dto/filter.dto.ts
@@ -1,4 +1,13 @@
+import { IsInt, Max, Min } from 'class-validator';
+
 export class FilterBodyDto {
+  @IsInt({})
+  @Min(0)
+  @Max(20)
   startId: number;
+
+  @IsInt({})
+  @Min(0)
+  @Max(20)
   endId: number;
 }
